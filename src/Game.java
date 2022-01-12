1bc4f949127af0574b2b9cee2dab0fc3d9316fb1
Christopher Fanning <chris@christopherfanning.com>
2022-01-11 21:08:16 -0600

    Some basic parts

--- src/Game.java
+++ src/Game.java
@@ -6,13 +6,22 @@ public class Game {
 
     private ArrayList<String> gameHistory = new ArrayList<>();
     private int numberOfPlayers;
+    private Scanner s;
 
-    public void startGame(){
+    public Game() {
+        Scanner s = new Scanner(System.in);
+    }
+
+    public Game(Player player1, Player player2) {
+        Scanner s = new Scanner(System.in);
+    }
+
+    public void startGame() {
         displayMenu();
     }
 
 
-    private void displayMenu(){
+    private void displayMenu() {
         // Logic for showing the menu.
 
         System.out.println("MAIN MENU");
@@ -20,16 +29,18 @@ public class Game {
         System.out.println("1. Type 'play' to play.");
         System.out.println("2. Type 'history' to view your game history.");
         System.out.println("3. Type 'quit' to stop playing.");
-        Scanner s = new Scanner(System.in);
 
         String menuChoice = s.nextLine();
         System.out.println(menuChoice);
-        if ( menuChoice.equals("play") ){
+        if (menuChoice.equals("play")) {
             // play the game
-        } else if ( menuChoice.equals("history") ){
+        } else if (menuChoice.equals("history")) {
             // goto the history page.
-        } else if ( menuChoice.equals("quit") ){
+        } else if (menuChoice.equals("quit")) {
             // run the quit routine.
+        } else if (menuChoice.equals("g")) {
+            System.out.println(computerPlayer());
+
         } else {
             System.out.println("Please, enter a valid choice. 'play', 'history' or 'quit' ");
         }
@@ -37,9 +48,66 @@ public class Game {
     }
 
 
-    private void playGame(){
+    private void playGame() {
+
+
+        int computerChoice = computerPlayer();
+
+        System.out.println("Choose your weapon before entering battle...");
+        System.out.println("'scissors', 'paper', 'rock'");
+
+
+        String playerChoice = s.nextLine();
+
+        if (playerChoice.equals("rock")) {
+
+            if (computerChoice == 3){
+                playerWins();
+            }else if (computerChoice == 2){
+                computerWins();
+            } else {
+                tieGame();
+            }
+
+        } else if (playerChoice.equals("paper")) {
+            if (computerChoice == 1){
+                playerWins();
+            }else if (computerChoice == 3){
+                computerWins();
+            }
+
+        } else if (playerChoice.equals("scissors")) {
+
+            if (computerChoice == 2){
+                playerWins();
+            }else if (computerChoice == 1){
+                computerWins();
+            }
+        } else {
+            playGame();
+        }
 
     }
 
+
+    private int computerPlayer() {
+        double a = Math.random() * 3;
+        return (int) a + 1;
+    }
+
+
+    private void playerWins(){
+        // The player wins;
+        System.out.println("The player wins!");
+
+    }
+
+    private void computerWins(){
+       // The computer overlords win.
+
+        System.out.println("The computer overlords win.");
+    }
+
+    private void tieGame(){}
 
 }
